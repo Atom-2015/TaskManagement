d9ec9f7d88868ef3b2697819e6125a34075898e8
Ankithooda1314 <ankithooda1314@gmail.com>
2025-01-13 11:04:57 +0530

    pushiinng code

--- myapp/src/Component/Dashboard/componentDashboard/clockDashboard.js
+++ myapp/src/Component/Dashboard/componentDashboard/clockDashboard.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 function ClockDashboard() {
   const [time, setTime] = useState(new Date());
   const name = "Aditya"; // You can replace this with dynamic user data
+  const [greet, setGreet] = useState('');
 
   useEffect(() => {
     // Update the time every second
@@ -10,20 +11,34 @@ function ClockDashboard() {
       setTime(new Date());
     }, 1000);
 
+    // Determine the greeting based on the current hour
+    const currentHour = time.getHours();
+    if (currentHour >= 5 && currentHour < 12) {
+      setGreet('Morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreet('Afternoon');
+    } else {
+      setGreet('Evening');
+    }
+
     return () => clearInterval(timer);
-  }, []);
+  }, [time]);
 
   return (
-    <div className="flex justify-between items-center  h-[120px] w-full p-4 mt-[50px] ">
+    <div className="flex justify-between items-center h-[120px] w-full p-4 mt-[50px]">
       {/* Greeting Section (left side) */}
-      <div className="text-2xl font-semibold text-gray-800  ">
-        Good Evening, {name}!
+      <div className="text-2xl font-semibold text-gray-800">
+        Good {greet}, {name}!
       </div>
 
       {/* Time Section (right side) */}
-      <div className=" shadow-md rounded-xl p-6 text-center w-[250px] bg-[#619FD0] h-[90px] ">
+      <div className="shadow-md rounded-xl p-6 text-center w-[250px] bg-[#619FD0] h-[90px]">
         <h2 className="text-2xl font-semibold text-gray-800">
-          {time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+          {time.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+          })}
         </h2>
         <p className="text-sm text-gray-600">
           {time.toLocaleDateString([], {
