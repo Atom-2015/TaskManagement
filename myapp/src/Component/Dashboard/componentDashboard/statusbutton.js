1609682bc892054cc1cd6ebb4939ae9bd16c93fd
Ankithooda1314 <ankithooda1314@gmail.com>
2025-01-20 19:29:14 +0530

    pulling code

---
+++ myapp/src/Component/Dashboard/componentDashboard/statusbutton.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const StatusButton = () => {
+  return (
+    <button
+      className={` text-center text-xs font-[14px] px-2 py-1 rounded `}
+    //   onClick={() => openModal(task)}
+    >
+    Update Task
+    </button>
+  );
+};
+
+export default StatusButton;
