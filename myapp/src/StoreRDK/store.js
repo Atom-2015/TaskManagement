93278ddb991fea579a98531bc34a5954f119cac4
adityapanday <aditya.panday.1986.ad@gmail.com>
2025-05-29 12:17:44 +0530

    jd

--- myapp/src/StoreRDK/store.js
+++ myapp/src/StoreRDK/store.js
@@ -12,7 +12,7 @@ import updateUser from '../FeatureRedux/user/updateUser_slice'; // Import the re
 import addtask  from '../FeatureRedux/task/addtaskSlice';
 import addsubtask from '../FeatureRedux/subTaskSlices/addsubTaskslice';
 import projectdetails from '../FeatureRedux/projectSlice/detailproject';
-import getsubtasklist from '../FeatureRedux/subTaskSlices/getsubtaskslice'
+import getsubtasklist from '../FeatureRedux/subTaskSlices/getsubtaskslice';
 import { editsubtasklist } from '../FeatureRedux/subTaskSlices/editusbTaskslice';
 import deletesubTaskslice from '../FeatureRedux/subTaskSlices/deletesubTaskslice';
 import addCompanySlice  from '../FeatureRedux/companySlice/addCompanyslice';
@@ -28,13 +28,8 @@ import  editRevenue  from '../FeatureRedux/RevenueSlice/editRevenueSlice';
 import addExpense  from '../FeatureRedux/expenseSlice/addExpenseSlice';
 import  editExpense  from '../FeatureRedux/expenseSlice/editExpenseSlice';
 import  getExpense  from '../FeatureRedux/expenseSlice/getExpenseSlice';
-<<<<<<< HEAD
-import  getClientDis  from '../FeatureRedux/ClientDisSlice/getClientdisSlice';
-import  addClientDis  from '../FeatureRedux/ClientDisSlice/addClientdisSlice';
-=======
 import  getExpenseDiscussion  from '../FeatureRedux/expenceDiscussionSlice/getExpenceDiscussion';
 import  addExpenceDiscussion from '../FeatureRedux/expenceDiscussionSlice/createExpenceDiscussionSlice';
->>>>>>> 0ebb430b647305bca78717a37b092ce0472cca59
 
 
 
@@ -69,15 +64,12 @@ export const store = configureStore({
         addExpense:addExpense,
         editExpense:editExpense,
         getExpense:getExpense,
-<<<<<<< HEAD
-        getClientDis:getClientDis,
-        addClientDis:addClientDis
-
-
-
-=======
-        getExpenseDiscussion:getExpenseDiscussion,
         addExpenceDiscussion:addExpenceDiscussion,
->>>>>>> 0ebb430b647305bca78717a37b092ce0472cca59
+        getExpenseDiscussion:getExpenseDiscussion,
+        // getClientDis:getClientDis,
+        // addClientDis:addClientDis
+
+
+
     },
 });
